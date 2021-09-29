02fa5742be1f8adafdcbead28229074388a232c9
ninjafiveo <42717162+ninjafiveo@users.noreply.github.com>
2021-09-29 10:26:20 -0400

    Added method to call a player from the NewPlayer List and add points to their score. Each time you call it, it adds 1 point to which ever player number you choose.

--- Nerd_Labyrinth_Final/Program.cs
+++ Nerd_Labyrinth_Final/Program.cs
@@ -11,7 +11,27 @@ namespace Nerd_Labyrinth_Final
         static void Main(string[] args)
         {
             Game_Start.Intro_To_Game();
+            Console.WriteLine("Below is the player check()");
+            Game_Start.Player_Check();
+            AddPoints(1);
+            AddPoints(1);
+            AddPoints(1);
+            AddPoints(2);
+            AddPoints(1);
             Console.ReadKey();
+        }
+
+        public static int AddPoints(int playerNumber)
+        {
+
+            Console.WriteLine($"{Game_Start.NewPlayer[playerNumber].Name} current score is: {Game_Start.NewPlayer[playerNumber].PlayerScore}");
+
+            Game_Start.NewPlayer[playerNumber].PlayerScore = Game_Start.NewPlayer[playerNumber].PlayerScore + 1;
+
+            Console.WriteLine($"{Game_Start.NewPlayer[playerNumber].Name} New Score is now:  {Game_Start.NewPlayer[playerNumber].PlayerScore}");
+
+            return Game_Start.NewPlayer[playerNumber].PlayerScore;
+
         }
     }
 }
